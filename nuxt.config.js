2270eb8b40665c6379544f8f7b4ebbfe47bbd138
“Leonardo <leonardo.edenirgraciano@gmail.com>
2022-04-29 06:53:44 -0300

    base do projeto

---
+++ nuxt.config.js
@@ -0,0 +1,65 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'LeosinhoFilmes',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    '@nuxtjs/axios'
+  ],
+
+  axios: {
+    baseURL: "https://api.themoviedb.org/3/",
+    config: {
+      "api_key": "7a6c316575389afeedb5d6c5dd145032",
+      "language": "pt-BR"
+    }
+  },
+
+  env: {
+    paramsUrl: {
+      api_key: "7a6c316575389afeedb5d6c5dd145032",
+      language: "pt-BR"
+    },
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
